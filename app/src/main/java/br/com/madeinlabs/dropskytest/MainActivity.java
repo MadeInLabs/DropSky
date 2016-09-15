57a74e2c052b6b29d25b71616f00aa3e1259743a
luisfelipeas5 <CezamoAbre_1995>
2016-09-14 21:00:05 -0300

    refactored

--- app/src/main/java/br/com/madeinlabs/dropskytest/MainActivity.java
+++ app/src/main/java/br/com/madeinlabs/dropskytest/MainActivity.java
@@ -3,8 +3,6 @@ package br.com.madeinlabs.dropskytest;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.DecelerateInterpolator;
